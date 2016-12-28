f646173656dbc754a36aa8de998755296386fcc8
Philippe Ehlert <philippe.ehlert@gmail.com>
2016-12-28 14:00:19 -0200

    Atividades da aula 4

---
+++ app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,35 @@
+package br.com.alura.agenda.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.io.Serializable;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDAO;
+
+public class SMSReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        String formato = (String) intent.getSerializableExtra("format");
+
+        SmsMessage sms = SmsMessage.createFromPdu(pdu, formato);
+
+        String telefone = sms.getDisplayOriginatingAddress();
+
+        AlunoDAO dao = new AlunoDAO(context);
+        if(dao.ehAluno(telefone)){
+            Toast.makeText(context, "Chegou um SMS", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+        dao.close();
+    }
+}
